d68035cb9830394304a9833bffd735fe43623ec7
akashGg99 <aakkash15r@gmail.com>
2023-01-08 21:22:31 +0530

    created rough page components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { AddBookForm } from './components/AddBookForm';
+import { BookDetail } from './components/bookdetail';
+// import { BookList } from './components/BookList';
+import { Login } from './components/loginPage';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <hr/>
+      <Login></Login>
+      <hr/>
+      <AddBookForm/>
+      <hr/>
+      <BookDetail/>
+      <hr/>
+      {/* <BookList/> */}
+    </>
   );
 }
 
